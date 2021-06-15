fa2b21b80e2a8d052d457653c1e3c257b2439fc5
KenChan413 <85539296+KenChan413@users.noreply.github.com>
2021-06-15 16:46:59 +0900

    CSS実装

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,23 @@ import "./styles.css";
 export const App = () => {
   return (
     <>
-      <div>
+      <div className="input-area">
         <input placeholder="TODOを入力" />
         <button>追加</button>
       </div>
-      <div>
-        <p>未完了のTODO</p>
+      <div className="incomplete-area">
+        <p className="title">未完了のTODO</p>
         <ul>
           <li>
-            <div>
-              <p>あああ</p>
+            <div className="list-row">
+              <p className="todo-area">あああ</p>
               <button>完了</button>
               <button>削除</button>
             </div>
           </li>
           <li>
-            <div>
-              <p>いいい</p>
+            <div className="list-row">
+              <p className="todo-area">いいい</p>
               <button>完了</button>
               <button>削除</button>
             </div>
@@ -28,12 +28,12 @@ export const App = () => {
         </ul>
       </div>
 
-      <div>
-        <p>完了のTODO</p>
+      <div className="complete-area">
+        <p className="title">完了のTODO</p>
         <ul>
           <li>
-            <div>
-              <p>ううう</p>
+            <div className="list-row">
+              <p className="todo-area">ううう</p>
               <button>戻す</button>
             </div>
           </li>
